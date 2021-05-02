4db1745da26bbca5af803f243e929126677ac308
Robert <rking92@gmail.com>
2021-05-01 23:00:41 -0400

    commit tree 1f497b37ae0256d8a2a81ed34e3e78dcb3cecfb8

---
+++ index.js
@@ -0,0 +1,11 @@
+let num1 = 2000
+let num2 = 100
+const divide = function() {
+    return num1 / num2;
+}
+divide();
+
+
+const square = (num3) => num3 * num3;
+
+const add = (num4, num5) => num4 + num5;
